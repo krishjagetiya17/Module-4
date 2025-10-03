4d9573b4169317f56c314ec57e4df72fa396452f
Krish Jagetiya <167842887+krishjagetiya17@users.noreply.github.com>
2025-10-04 00:10:28 +0530

    Add files via upload

---
+++ Template/2_sort_array_using_template.cpp
@@ -0,0 +1,45 @@
+//Write a program of to sort the array using templates
+#include <iostream>
+using namespace std;
+
+template <class t>
+void arry(t arr[], int size) 
+{
+    t temp;
+    cout << "\n\n\t Before Sorting...";
+    for(int i = 0; i < size; i++) 
+    {
+        cout << "\n\n\t Array Element [" << i << "] : " << arr[i];
+    }
+    
+    for(int i = 0; i < size; i++) 
+    {
+        for(int j = i + 1; j < size; j++) 
+        {
+            if(arr[i] > arr[j]) 
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }	
+        }
+    }
+    
+    cout << "\n\n\t After Sorting...";
+    for(int i = 0; i < size; i++) 
+    {
+        cout << "\n\n\t Array Element [" << i << "] : " << arr[i];
+    }
+}
+
+int main() 
+{
+    int arr[5];
+    for(int i = 0; i < 5; i++) 
+    {
+        cout << "\n\n\t Input array element [" << i << "] : ";
+        cin >> arr[i];
+    }
+    arry(arr, 5);
+
+}
